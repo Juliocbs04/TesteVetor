dd59c7108058fd12513e4899d513472da3a96767
IFSC <IFSC@CDR-770655.CEFETSC>
2018-03-28 20:30:46 -0300

    Ultima versao

--- src/aplicacao/Aplicacao.java
+++ src/aplicacao/Aplicacao.java
@@ -1,8 +1,11 @@
 package aplicacao;
 public class Aplicacao {
     public static void main(String[] args) {
-    
-        
+        Ordenar v = new Ordenar(10);
+        v.randomFill();
+        v.imprimeVetor();
+        v.bubbleSort();
+        v.imprimeVetor();
         
     }
     
--- src/aplicacao/Ordenar.java
+++ src/aplicacao/Ordenar.java
@@ -3,7 +3,7 @@ package aplicacao;
 import java.util.Random;
 
 public class Ordenar {
-    int [] vetor;
+    private int [] vetor;
     // declare o tamanho do vetor;
     public Ordenar(int size){
         vetor = new int[size];
@@ -14,22 +14,40 @@ public class Ordenar {
         Random gerador = new Random();
         
         for(int i=0;i<vetor.length;i++){
-            vetor[i]= gerador.nextInt(10000);
+            vetor[i]= gerador.nextInt(100);
         }
     }
     //Ordenação do vetor
     public void bubbleSort(){
-        
+        int aux;
+        for(int i=0;i<vetor.length;i++){
+            for(int j=i+1;j<vetor.length;j++){
+                if(vetor[i]>vetor[j]){
+                   aux=vetor[i];
+                   vetor[i]=vetor[j];
+                   vetor[j]=aux;
+                }
+            }
+        }
     }
     //converte o vetor para String
+    /*
     public String toString(){
         
-      
+        
+    }
+    */
+    public void imprimeVetor(){
+        for(int i=0;i<vetor.length;i++){
+            System.out.printf("\n %d %d",vetor[i],i);
+        }
     }
     
     public boolean equals(int [] v){
         
+        return true;
     }
+    
     
     public void setAt(int pos, int value){
         //Atribui um valor na posição pos do vetor
@@ -38,14 +56,20 @@ public class Ordenar {
     public int valueAt(int pos){
         
         //retorna o valor na posição pos do vetor
+        return 0;
     }
+    
     
     public int lenght(){
-        // retorna o tamanho do vetor
+        int tamanho=0;
+        tamanho= vetor.length;
+        return tamanho;
     }
     
-    public Vetor Concat(Vetor v){
+    public int[] Concat(int [] vetor){
         
+        return vetor;
+        //return 0;
     }
     
     
