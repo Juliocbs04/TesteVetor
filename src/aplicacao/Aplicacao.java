5295a0c5a96adb78c1268a07351d62d26c670d76
IFSC <IFSC@CDR-770655.CEFETSC>
2018-03-28 22:26:53 -0300

    Ultima Versao2

--- src/aplicacao/Aplicacao.java
+++ src/aplicacao/Aplicacao.java
@@ -2,10 +2,21 @@ package aplicacao;
 public class Aplicacao {
     public static void main(String[] args) {
         Ordenar v = new Ordenar(10);
+        Ordenar v2= new Ordenar(10);
+        //vetor 1
         v.randomFill();
-        v.imprimeVetor();
+        //v.imprimeVetor();
         v.bubbleSort();
-        v.imprimeVetor();
+        //v.setAt(10, 566);
+        //v.valueAt(12);
+        //v.imprimeVetor();
+        if(v.equals(v2)){
+            System.out.println("\n Os vetores são iguais");
+        }else{
+              System.out.println("\n Os vetores são diferentes");
+        }
+        //System.out.println("o tamanho eh"+ v.length());
+        v.Concat(v2);
         
     }
     
--- src/aplicacao/Ordenar.java
+++ src/aplicacao/Ordenar.java
@@ -4,8 +4,10 @@ import java.util.Random;
 
 public class Ordenar {
     private int [] vetor;
+    private int size;
     // declare o tamanho do vetor;
-    public Ordenar(int size){
+    public Ordenar(int novoSize){
+        size= novoSize;
         vetor = new int[size];
         
     }
@@ -31,45 +33,90 @@ public class Ordenar {
         }
     }
     //converte o vetor para String
-    /*
+
     public String toString(){
+        String str="";
         
-        
+        for(int i=0;i<vetor.length;i++){
+            str=str+" "+ String.valueOf(vetor[i]);
+        }
+        //Retorna os valores do vetor em forma de string
+        return str;
     }
-    */
+  
     public void imprimeVetor(){
         for(int i=0;i<vetor.length;i++){
-            System.out.printf("\n %d %d",vetor[i],i);
+            System.out.printf("\n %d",vetor[i]);
         }
     }
     
-    public boolean equals(int [] v){
+    public boolean equals(Ordenar v){
+        int flag=1;
+        for(int i=0;i<this.length();i++){
+                if(vetor[i]==v.valueAt(i)){
         
-        return true;
+                }else{
+                    flag=0;
+                   
+                }
+            }
+        if(flag==0){
+            return false;
+        }else{
+            return true;
+        }
+        
     }
     
     
     public void setAt(int pos, int value){
-        //Atribui um valor na posição pos do vetor
+        if(pos>=size){
+            System.out.println("Essa posição não existe no Vetor!");
+        }else{
+            vetor[pos]=value;    
+        }
+       
     }
     
     public int valueAt(int pos){
-        
+        if(pos>=size){
+            System.out.println("\n Essa posição não existe no Vetor!");
+        }else{
+            System.out.printf("O valor da posição é: %d",vetor[pos]);    
+        }
         //retorna o valor na posição pos do vetor
-        return 0;
+        return vetor[pos];
     }
     
     
-    public int lenght(){
+    public int length(){
         int tamanho=0;
         tamanho= vetor.length;
         return tamanho;
     }
     
-    public int[] Concat(int [] vetor){
+    public int[] Concat(int [] vetor2){
+        int [] resultado= new int[vetor.length+vetor2.length];
+        int i;
+        for(i=0;i<vetor.length;i++){
+            resultado[i]= vetor[i];
+        }
         
-        return vetor;
-        //return 0;
+        for(;i<vetor.length+vetor2.length;i++){
+            resultado[i]= vetor2[i];
+        }
+        
+        
+        
+        return resultado;
+    }
+
+    int[] Concat(Ordenar v2) {
+        int [] resultado = new int[this.length()+v2.length()];
+        
+        for(int i=this.length();i<this.length()+v2.length();i++){
+            resultado[i]= v2.valueAt(i);
+        }
     }
     
     
